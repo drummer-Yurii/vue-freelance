203a66e65dcc342d066b663e4a07a3acc68fede1
Yurii Rybachok <drummer2009@ukr.net>
2023-10-03 17:48:31 +0300

    create project & setup router

---
+++ src/router.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory,
+  routes: [
+    { path: '/devs', component: null },
+    { path: '/devs/:id', component: null, children: [{ path: '/contact', component: null }] },
+    { path: '/register', component: null },
+    { path: '/requests', component: null },
+    { path: '/:notFound(.*)', component: null },
+  ],
+});
+
+export default router;
