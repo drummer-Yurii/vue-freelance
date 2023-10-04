c9dcea348fa31284ac1cefa31f3dd9633968ee28
Yurii Rybachok <drummer2009@ukr.net>
2023-10-04 10:48:20 +0300

    show devs in list & show msg when dont have devs

--- src/pages/devs/DevList.vue
+++ src/pages/devs/DevList.vue
@@ -1,19 +1,30 @@
 <template>
-  <section>
-    FILTER
-  </section>
+  <section>FILTER</section>
   <section>
     <div class="controls">
       <button>Refresh</button>
       <router-link to="/register">Register as a Developer</router-link>
     </div>
-    LIST OF DEVS
+    <ul v-if="hasDev">
+      <li v-for="dev in filteredList" :key="dev.id">
+        {{ dev.firstName }}
+      </li>
+    </ul>
+    <p v-else>No developers found</p>
   </section>
 </template>
 
 <script>
 export default {
   name: 'DevList',
+  computed: {
+    filteredList() {
+      return this.$store.getters['devModule/devs'];
+    },
+    hasDev() {
+      return this.$store.getters['devModule/hasDev'];
+    }
+  },
 };
 </script>
 
--- src/store/modules/devs/getters.js
+++ src/store/modules/devs/getters.js
@@ -1,1 +1,8 @@
-export default {};
+export default {
+  devs(state) {
+    return state.devs;
+  },
+  hasDev(state) {
+    return state.devs && state.devs.length > 0;
+  },
+};
