6e9256ac50586a649d4aa885d7f5a5535d0aa622
Yurii Rybachok <drummer2009@ukr.net>
2023-10-04 11:12:51 +0300

    create devItem component & use him in list

---
+++ src/pages/components/DevItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <li>
+    <h3>{{ fullName }}</h3>
+    <h4>$ {{ rate }}/hour</h4>
+  </li>
+  <span v-for="area in areas" :key="area.id">{{ area }}</span>
+  <div class="actions">
+    <router-link :to="devContactLink">Contact</router-link>
+    <router-link :to="devDetailsLink">ViewDetails</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DevItem',
+  props: ['id', 'firstName', 'lastName', 'rate', 'areas'],
+  computed: {
+    fullName() {
+      return this.firstName + ' ' + this.lastName;
+    },
+    devContactLink() {
+      return '/devs/' + this.id + '/contact';
+    },
+    devDetailsLink() {
+      return '/devs/' + this.id;
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/pages/devs/DevList.vue
+++ src/pages/devs/DevList.vue
@@ -6,16 +6,25 @@
       <router-link to="/register">Register as a Developer</router-link>
     </div>
     <ul v-if="hasDev">
-      <li v-for="dev in filteredList" :key="dev.id">
-        {{ dev.firstName }}
-      </li>
+      <DevItem
+        v-for="dev in filteredList"
+        :key="dev.id"
+        id="dev.id"
+        :first-name="dev.firstName"
+        :last-name="dev.lastName"
+        :rate="dev.hourlyRate"
+        :areas="dev.areas"
+      >
+      </DevItem>
     </ul>
     <p v-else>No developers found</p>
   </section>
 </template>
 
 <script>
+import DevItem from '../components/DevItem';
 export default {
+  components: { DevItem },
   name: 'DevList',
   computed: {
     filteredList() {
@@ -23,9 +32,20 @@ export default {
     },
     hasDev() {
       return this.$store.getters['devModule/hasDev'];
-    }
+    },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
