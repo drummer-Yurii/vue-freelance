1938651ba2f3daf3d46d588c5c365960af10a765
Yurii Rybachok <drummer2009@ukr.net>
2023-10-06 11:48:21 +0300

    create ContactDev component

--- src/pages/requests/ContactDev.vue
+++ src/pages/requests/ContactDev.vue
@@ -1,11 +1,68 @@
 <template>
-  <div>CONTACT PAGE</div>
+  <form @submit.prevent="submitForm">
+    <div class="form-control">
+      <label for="email">Your Email</label>
+      <input type="text" id="email" v-model="email.trim" />
+    </div>
+    <div>
+      <label for="message">Message</label>
+      <textarea id="message" rows="5" v-model="message.trim"></textarea>
+    </div>
+    <div class="actions">
+      <BaseButton>Send Message</BaseButton>
+    </div>
+    <p class="errors" v-if="!formIsValid">Please enter a valid email and non-empty message</p>
+  </form>
 </template>
 
 <script>
+import BaseButton from '../components/ui/BaseButton';
 export default {
+  components: { BaseButton },
   name: 'ContactDev',
+  data() {
+    return {
+      email: '',
+      message: '',
+      formIsValid: true,
+    };
+  },
+  methods: {
+    submitForm() {
+      this.formIsValid = true;
+      if (this.email === '' || !this.includes('@') || this.message === '') {
+        this.formIsValid = false;
+        return;
+      }
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+</style>
