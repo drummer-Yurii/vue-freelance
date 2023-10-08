396c66edf26997704e8972066232ce74fbf85f2d
Yurii Rybachok <drummer2009@ukr.net>
2023-10-08 12:01:50 +0300

    save requests in db

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,23 @@
 export default {
-  contactDev(context, payload) {
+  async contactDev(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      devId: payload.devId,
       userEmail: payload.email,
       message: payload.message,
     };
+    const response = await fetch(
+      `https://app-vue-c4cfa-default-rtdb.firebaseio.com/requests/${payload.devId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      },
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to request');
+      throw error;
+    }
+    newRequest.id = responseData.name;
+    newRequest.devId = payload.devId;
     context.commit('addRequest', newRequest);
   },
 };
