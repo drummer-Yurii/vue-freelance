e145cf9022dbcdd80db118ac14e66b3fa1817647
Yurii Rybachok <drummer2009@ukr.net>
2023-10-08 10:42:12 +0300

    create NotFoud component

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,9 +1,16 @@
 <template>
-  <div></div>
+  <BaseCard>
+    <section>
+      <h2>Page not found!</h2>
+      <p>This page could not be found! Maybe check out all our <router-link to="/devs">developers</router-link></p>
+    </section>
+  </BaseCard>
 </template>
 
 <script>
+import BaseCard from './components/ui/BaseCard'
 export default {
+  components: { BaseCard },
   name: 'NotFound',
 };
 </script>
