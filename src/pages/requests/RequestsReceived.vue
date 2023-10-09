cb12b79e03595605f68ab8810892d2e2b3b32b0a
Yurii Rybachok <drummer2009@ukr.net>
2023-10-09 09:38:17 +0300

    catch error in requests

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,31 +1,47 @@
 <template>
-  <section>
-    <BaseCard>
-      <header>
-        <h2>Requests Received</h2>
-      </header>
-      <ul v-if="hasRequests">
-        <RequestItem
-          v-for="req in receivedRequests"
-          :key="req.id"
-          :email="req.userEmail"
-          :message="req.message"
-        ></RequestItem>
-      </ul>
-      <h3 v-else>You haven't received any requests yet</h3>
-    </BaseCard>
-  </section>
+  <div>
+    <BaseDialog :show="!!error" title="An error occurred!" @close="handleError">
+      <p>{{ error }}</p>
+    </BaseDialog>
+    <section>
+      <BaseCard>
+        <header>
+          <h2>Requests Received</h2>
+        </header>
+        <BaseSpinner v-if="isLoading"></BaseSpinner>
+        <ul v-else-if="hasRequests">
+          <RequestItem
+            v-for="req in receivedRequests"
+            :key="req.id"
+            :email="req.userEmail"
+            :message="req.message"
+          ></RequestItem>
+        </ul>
+        <h3 v-else>You haven't received any requests yet</h3>
+      </BaseCard>
+    </section>
+  </div>
 </template>
 
 <script>
+import BaseSpinner from '../components/ui/BaseSpinner';
+import BaseDialog from '../components/ui/BaseDialog';
 import RequestItem from './RequestItem';
 import BaseCard from '../components/ui/BaseCard';
 export default {
   components: {
+    BaseSpinner,
+    BaseDialog,
     RequestItem,
     BaseCard,
   },
   name: 'RequestsReseived',
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
+  },
   computed: {
     receivedRequests() {
       return this.$store.getters['requestModule/requests'];
@@ -34,12 +50,21 @@ export default {
       return this.$store.getters['requestModule/hasRequests'];
     },
   },
-  created () {
+  created() {
     this.loadRequests();
   },
   methods: {
-    loadRequests() {
-      return this.$store.dispatch('requestModule/fetchRequests');
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requestModule/fetchRequests');
+      } catch (error) {
+        this.error = error.message || 'Something failed';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     },
   },
 };
