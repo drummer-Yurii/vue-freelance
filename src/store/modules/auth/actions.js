ee9b86d3b5255138ad8888bed139a5cd5a887ed1
Yurii Rybachok <drummer2009@ukr.net>
2023-10-10 15:07:46 +0300

    create login of user

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,30 @@
 export default {
-//   login(context, payload) {},
+  async login(context, payload) {
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAngSMfNudC_FLg7J58NjZj-CupcBWYiVQ`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnServiceToken: true,
+        }),
+      },
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      console.log(response);
+      const error = new Error(responseData.message || 'Failed to fetch');
+      throw error;
+    }
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+
   async signup(context, payload) {
     const response = await fetch(
       `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAngSMfNudC_FLg7J58NjZj-CupcBWYiVQ`,
